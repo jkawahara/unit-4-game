0f2ae0a79bffe210bf091ccecd98e72db7e43302
John Kawahara <kawaharajohn@gmail.com>
2018-11-10 19:09:17 -0800

    Create static display

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+  // VARIABLES
+  // =========
+
+
+
+
+
+
+
+  // FUNCTIONS
+  // =========
+
+  function displayUpdate() {
+
+  }
+
+  function restartGame() {
+
+  }
+
+  $(".btn").on("click", function() {
+    console.log($(this).attr("value"));
+
+  });
+
+});
