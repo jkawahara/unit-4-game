aef1a610a9d619c268ee409f9bd3514230a421f2
John Kawahara <kawaharajohn@gmail.com>
2018-11-12 19:53:40 -0800

    Prep static UI; plan JS variables, functions

---
+++ assets/javascript/game2.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+  // GLOBAL VARIABLES
+  // =========
+
+
+
+  // FUNCTIONS
+  // =========
+
+  // Update dispaly function
+  function displayUpdate() {
+
+  }
+
+  // Restart game function
+  function restartGame() {
+
+  }
+
+  // MAIN CONTROLLER
+  restartGame();
+  displayUpdate();
+
+  // Event listener
+  $(".btn").on("click", function() {
+    
+  });
+
+});
